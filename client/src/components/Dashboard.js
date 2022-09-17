4413b9931c288cbd12b4fa622f90dbce605d096e
taylankalkan01 <e.taylankalkan@hotmail.com>
2022-09-17 00:48:15 -0500

    Contact page added.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./Main";
-import Container from "react-bootstrap/esm/Container";
 import ProfilePrivateRoutes from "../constants/ProfilePrivateRoute";
 import NotesPrivateRoutes from "../constants/NotesPrivateRoutes";
 import LoginPrivateRoutes from "../constants/LoginPrivateRoutes";
@@ -9,40 +8,42 @@ import RegisterPrivateRoutes from "../constants/RegisterPrivateRoutes";
 import CreateNotePrivateRoutes from "../constants/CreateNotesPrivateRoutes";
 import NotFound from "./NotFound";
 import EditNote from "./EditNote";
+import Contact from "./Contact";
 
 export default function Dashboard() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Main></Main>}></Route>
-        <Route
-          exact
-          path="/login"
-          element={<LoginPrivateRoutes></LoginPrivateRoutes>}
-        ></Route>
-        <Route
-          exact
-          path="/register"
-          element={<RegisterPrivateRoutes></RegisterPrivateRoutes>}
-        ></Route>
-        <Route
-          exact
-          path="/profile"
-          element={<ProfilePrivateRoutes></ProfilePrivateRoutes>}
-        ></Route>
-        <Route
-          exact
-          path="/notes"
-          element={<NotesPrivateRoutes></NotesPrivateRoutes>}
-        ></Route>
-        <Route
-          exact
-          path="/createnote"
-          element={<CreateNotePrivateRoutes></CreateNotePrivateRoutes>}
-        ></Route>
-        <Route exact path="*" element={<NotFound></NotFound>}></Route>
-        <Route path="/edit/:id" element={<EditNote></EditNote>}></Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Main></Main>}></Route>
+				<Route
+					exact
+					path="/login"
+					element={<LoginPrivateRoutes></LoginPrivateRoutes>}
+				></Route>
+				<Route
+					exact
+					path="/register"
+					element={<RegisterPrivateRoutes></RegisterPrivateRoutes>}
+				></Route>
+				<Route
+					exact
+					path="/profile"
+					element={<ProfilePrivateRoutes></ProfilePrivateRoutes>}
+				></Route>
+				<Route
+					exact
+					path="/notes"
+					element={<NotesPrivateRoutes></NotesPrivateRoutes>}
+				></Route>
+				<Route
+					exact
+					path="/createnote"
+					element={<CreateNotePrivateRoutes></CreateNotePrivateRoutes>}
+				></Route>
+				<Route exact path="*" element={<NotFound></NotFound>}></Route>
+				<Route path="/edit/:id" element={<EditNote></EditNote>}></Route>
+				<Route path="/contact" element={<Contact />}></Route>
+			</Routes>
+		</div>
+	);
 }
