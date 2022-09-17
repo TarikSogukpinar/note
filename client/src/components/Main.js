9617f410966ef7aa56f22dfabbab028348335fef
TarıkSoğukpınar <hanluee@yandex.com.tr>
2022-09-17 10:41:51 +0300

    Footer component added!

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,8 +3,9 @@ import "../styles/Main.css";
 import Button from "react-bootstrap/Button";
 import { AiOutlineFileProtect } from "react-icons/ai";
 import { GiTakeMyMoney } from "react-icons/gi";
-import { SiFsecure } from "react-icons/si";
+import { SiFsecure, SiCrowdsource } from "react-icons/si";
 import { Col, Row } from "react-bootstrap";
+import Footer from "./Footer";
 
 export default function Main() {
   const auth = localStorage.getItem("token");
@@ -16,7 +17,7 @@ export default function Main() {
             <h1>Welcome to the note taking application</h1>
             <h4>
               This application is completely free. We encrypt your passwords and
-              all your notes. You can start{" "}
+              all your notes. You can start here{" "}
             </h4>
             <br></br>
             {!auth ? (
@@ -76,13 +77,11 @@ export default function Main() {
             </div>
             <div className="col-lg-6 order-lg-1">
               <div className="p-5">
-                <h2 className="display-4"> We Protect your data</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+                <p className="display-1"> We Protect Your Data</p>
+                <h4>
+                  We encrypt all the notes you save. Nobody, including us, has
+                  access to these notes.
+                </h4>
               </div>
             </div>
           </div>
@@ -99,13 +98,10 @@ export default function Main() {
             </div>
             <div className="col-lg-6">
               <div className="p-5">
-                <h2 className="display-4"> Free To Use!</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+                <h2 className="display-2"> Free To Use</h2>
+                <h4>
+                  Note App is completely free. No need to pay to save notes
+                </h4>
               </div>
             </div>
           </div>
@@ -122,18 +118,39 @@ export default function Main() {
             </div>
             <div className="col-lg-6 order-lg-1">
               <div className="p-5">
-                <h2 className="display-4"> Secure Registration!</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+                <p className="display-3"> Secure Registration</p>
+                <h4>
+                  We securely store your user password. You don't have to worry
+                  about it!
+                </h4>
               </div>
             </div>
           </div>
         </div>
       </section>
+
+      <section className="bg-dark text-light">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+            <div className="col-lg-6">
+              <div className="p-5">
+                <SiCrowdsource size={200} />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h2 className="display-2"> Open Source</h2>
+                <h4>
+                  This software is completely open source. You can support us on{" "}
+                  <a href="https://github.com/"> Github</a> and review the code.
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
     </div>
   );
 }
